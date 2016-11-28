8d622cb23a25c18b2d61783caede9a278d55c8f8
BluSunrize <danny.rad10@yahoo.de>
2016-11-28 17:43:54 +0100

    Attempted to add proper itemrenders. Failed miserably. This commit now serves as an introductory course to custom baked models. =P

--- src/main/java/xyz/brassgoggledcoders/opentransport/minecarts/renderers/RenderItemHolderMinecart.java
+++ src/main/java/xyz/brassgoggledcoders/opentransport/minecarts/renderers/RenderItemHolderMinecart.java
@@ -1,20 +1,130 @@
 package xyz.brassgoggledcoders.opentransport.minecarts.renderers;
 
+import blusunrize.immersiveengineering.common.util.chickenbones.Matrix4;
+import com.google.common.collect.Lists;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.ModelMinecart;
+import net.minecraft.client.renderer.block.model.*;
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms.TransformType;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.IPerspectiveAwareModel;
+import org.apache.commons.lang3.tuple.Pair;
+import xyz.brassgoggledcoders.opentransport.api.blockwrappers.IBlockWrapper;
 import xyz.brassgoggledcoders.opentransport.minecarts.entities.EntityMinecartHolder;
 import xyz.brassgoggledcoders.opentransport.renderers.RenderBlock;
 
-public class RenderItemHolderMinecart /*implements IItemRenderingHandler*/ {
-    private RenderBlock renderBlock;
-    private EntityMinecartHolder minecartHolder;
-    private ModelMinecart modelMinecart;
-    private ResourceLocation minecartTexture = new ResourceLocation("textures/entity/minecart.png");
+import javax.annotation.Nullable;
+import javax.vecmath.Matrix4f;
+import java.util.*;
+
+public class RenderItemHolderMinecart implements IBakedModel, IPerspectiveAwareModel {
+
+    //Basically, I made this an IBakedModel, since that is how you do ItemModels now.
+    //Ideally, you would have a "base" version of this, that handles all the block wrapper part and caching and shit
+    //And you'd make "Minecart" and "Boat" two subclasses that simply override a method like "getContainerQuads", which returns a list of quads that look like a cart/boat
+
+    Set<BakedQuad> bakedQuads;//This caches the baked quads, it's initialized during the first bake. If you had meta/nbt differences, you'd make this a map or smth and use the OverrideLists to handle it all
+    static List<BakedQuad> emptyQuads = Lists.newArrayList();//Empty quads list so you don't constantly initialize new lists
+
+    IBlockWrapper blockWrapper;//The blockwrapper contained
+
+    public RenderItemHolderMinecart(IBlockWrapper blockWrapper) {
+        this.blockWrapper = blockWrapper;
+    }
 
     public RenderItemHolderMinecart() {
-        renderBlock = new RenderBlock();
-        modelMinecart = new ModelMinecart();
+        this(null);
+//        renderBlock = new RenderBlock();
+//        modelMinecart = new ModelMinecart();
     }
+
+
+
+    @Override
+    public Pair<? extends IBakedModel, Matrix4f> handlePerspective(TransformType cameraTransformType)
+    {
+        Matrix4 matrix = new Matrix4();//currently usign IE/CodeChicken matrices here, since they have sensible translation methods.
+        //I would have switched it to vanilal Matrices later, once I had the values figured out
+matrix.translate(4,0,0);
+        return Pair.of(this, matrix.toMatrix4f());
+    }
+
+    @Override
+    public List<BakedQuad> getQuads(@Nullable IBlockState state, @Nullable EnumFacing side, long rand)
+    {
+        if(bakedQuads == null)//if they have already been baked, you can save the trouble. I frequently comment this out when testing stuff, so that it rebakes and updates
+        {
+            try{
+                bakedQuads = Collections.synchronizedSet(new LinkedHashSet<BakedQuad>());
+
+                Minecraft mc = Minecraft.getMinecraft();
+                //This is where the "getContainerQuads()" would sit, that adds either a cart or a boat to the list of quads.
+                //It will of course need a texture stitched into the sheet, but you can probably take hte vanilla cart texture and cut it down+optimize it to fit in a 32x32
+                //You wouldn't see a lot of the insides so you can get lazy with it.
+                TextureAtlasSprite cartTexture = mc.getTextureMapBlocks().getAtlasSprite("minecraft:blocks/diamond_ore");
+
+
+
+                //Get quads for the contained Block
+                IBakedModel blockModel = mc.getBlockRendererDispatcher().getBlockModelShapes().getModelForState(blockWrapper.getBlockState());
+                HashSet<BakedQuad> set = new HashSet();
+                //From experience and also what AtomicBlom had to do for chiselled sheep. You have to check quads for all facings and null.
+                //I used a set in the hopes of quads hashing well and possibly avoiding duplicates with this.
+                for(EnumFacing f : EnumFacing.values())
+                    set.addAll(blockModel.getQuads(blockWrapper.getBlockState(), f, 0));
+                //I added this if check as an experiment. Might work, might not. Might have to always query null as well, not sure.
+                if(set.isEmpty())
+                    set.addAll(blockModel.getQuads(blockWrapper.getBlockState(), null, 0));
+
+                bakedQuads.addAll(set);
+
+            }catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+        if(bakedQuads!=null && !bakedQuads.isEmpty())
+        {
+            //Never return the original list, always make a synchronized copy.
+            //Or at least that is what everyone else does, so I stuck with what I assume is convention.
+            List<BakedQuad> quadList = Collections.synchronizedList(Lists.newArrayList(bakedQuads));
+            return quadList;
+        }
+        return emptyQuads;
+    }
+
+    @Override
+    public boolean isAmbientOcclusion() {
+        return true;
+    }
+    @Override
+    public boolean isGui3d() {
+        return true;
+    }
+    @Override
+    public boolean isBuiltInRenderer() {
+        return false;
+    }
+    @Override
+    public TextureAtlasSprite getParticleTexture() {
+        return null;
+    }
+    @Override
+    public ItemCameraTransforms getItemCameraTransforms() {
+        return ItemCameraTransforms.DEFAULT;
+    }
+
+    @Override
+    //This would be used if you have to differ items by meta or nbt data, since the normal GetQuads doesn't feature it.
+    //IE's ModelCoresample is a good example for how that can be handled.
+    public ItemOverrideList getOverrides() {
+        return null;
+    }
+
+
 /*TODO: RENDERING CODE
     @Override
 	public void render(World world, Item item, ItemStack itemStack, TransformType type) {
--- src/main/java/xyz/brassgoggledcoders/opentransport/proxies/ClientProxy.java
+++ src/main/java/xyz/brassgoggledcoders/opentransport/proxies/ClientProxy.java
@@ -2,17 +2,29 @@ package xyz.brassgoggledcoders.opentransport.proxies;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.IThreadListener;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
+import net.minecraftforge.client.event.ModelBakeEvent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+import xyz.brassgoggledcoders.opentransport.OpenTransport;
+import xyz.brassgoggledcoders.opentransport.api.blockwrappers.IBlockWrapper;
 import xyz.brassgoggledcoders.opentransport.api.entities.IHolderEntity;
+import xyz.brassgoggledcoders.opentransport.api.transporttypes.ITransportType;
 import xyz.brassgoggledcoders.opentransport.boats.entities.EntityBoatHolder;
 import xyz.brassgoggledcoders.opentransport.boats.renderers.RenderHolderBoat;
 import xyz.brassgoggledcoders.opentransport.minecarts.entities.EntityMinecartHolder;
 import xyz.brassgoggledcoders.opentransport.minecarts.renderers.RenderHolderMinecart;
+import xyz.brassgoggledcoders.opentransport.minecarts.renderers.RenderItemHolderMinecart;
+import xyz.brassgoggledcoders.opentransport.registries.BlockWrapperRegistry;
 import xyz.brassgoggledcoders.opentransport.wrappers.player.EntityPlayerSPWrapper;
+
+import java.util.Map.Entry;
 
 public class ClientProxy extends CommonProxy {
     @Override
@@ -37,5 +49,27 @@ public class ClientProxy extends CommonProxy {
     public void registerEntityRenders() {
         RenderingRegistry.registerEntityRenderingHandler(EntityBoatHolder.class, RenderHolderBoat::new);
         RenderingRegistry.registerEntityRenderingHandler(EntityMinecartHolder.class, RenderHolderMinecart::new);
+       //Register for events
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onModelBakeEvent(ModelBakeEvent event) {
+
+        //Iterate through them all
+        for(Entry<String, IBlockWrapper> entry : BlockWrapperRegistry.getAllBlockWrappers().entrySet()) {
+            IBlockWrapper blockWrapper = entry.getValue();
+            //create full registry names
+            String minecart = "minecart.holder." + blockWrapper.getUnlocalizedName();
+            String boat = "boat.holder." + blockWrapper.getUnlocalizedName();
+
+            //Create model location
+            //This is funky for you. You seem to have routed them all to "opentransport:minecart" somehow, rather than having separate jsons
+            //I couldn't figure out where you do that routign, but you will have to undo it probably to make this part work
+            ModelResourceLocation mLoc = new ModelResourceLocation(new ResourceLocation("opentransport", minecart), "inventory");
+            //Replace normal model with the new one, parsing in the blockwrapper
+            event.getModelRegistry().putObject(mLoc, new RenderItemHolderMinecart(blockWrapper));
+
+        }
     }
 }
